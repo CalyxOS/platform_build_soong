2e91c6a46f462b7822c6fd1f7fb83011793d2a07
Michael Bestas <mkbestas@gmail.com>
2025-06-12 22:30:50 +0300

    Allow Android.mk in external/google/talkback
    This is not actually used, so add it to the list of allowed directories
    
    Change-Id: Idcffccf84136061aed0b5a4c548fa66e01c8655e

--- ui/build/androidmk_denylist.go
+++ ui/build/androidmk_denylist.go
@@ -119,6 +119,7 @@ var ignore_androidmks []string = []string{
 	// wpa_supplicant_8 has been converted to Android.bp and Android.mk files are kept for troubleshooting.
 	"external/wpa_supplicant_8/",
 	// Empty Android.mk in package's top directory
+	"external/google/talkback/",
 	"external/proguard/",
 	"external/swig/",
 	"toolchain/",
